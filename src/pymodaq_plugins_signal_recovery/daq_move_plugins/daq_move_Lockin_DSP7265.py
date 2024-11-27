cd05193fe4e0edcf900e49a49f35b6a96ce4ef65
louisld <louisg@bloomenetwork.fr>
2024-11-27 12:36:00 +0100

    DSP 7265 daq_move parameters completion

--- src/pymodaq_plugins_signal_recovery/daq_move_plugins/daq_move_Lockin_DSP7265.py
+++ src/pymodaq_plugins_signal_recovery/daq_move_plugins/daq_move_Lockin_DSP7265.py
@@ -20,9 +20,20 @@ from easydict import EasyDict as edict
 
 import pyvisa
 
+def build_dict_from_float_list(time_constants: list[float], unit: str = "") -> dict:
+    d = {}
+    for tc in time_constants:
+        d[f"{tc:.2e} {unit}"] = tc
+    return d
+
 rm = pyvisa.ResourceManager()
 VISA_RESOURCES = rm.list_resources()
 ADAPTERS = dict(VISA=VISAAdapter, Prologix=PrologixAdapter)
+FET = {"Bipolar": 0, "FET": 1}
+SHIELD = {"Grounded": 0, "Floating": 1}
+COUPLING = {"AC": 0, "DC": 1}
+TIME_CONSTANTS = build_dict_from_float_list(DSP7265.TIME_CONSTANTS, "s")
+GAIN = list(range(0, 100, 10))
 
 class DAQ_Move_Lockin_DSP7265(DAQ_Move_base):
     """Plugin for the Signal Recovery DSP 7265 Instrument
@@ -37,11 +48,22 @@ class DAQ_Move_Lockin_DSP7265(DAQ_Move_base):
     params = [
         {'title': 'Adapter', 'name': 'adapter', 'type': 'list', 'limits': list(ADAPTERS.keys())},
         {'title': 'VISA Address:', 'name': 'address', 'type': 'list', 'limits': VISA_RESOURCES},
-        {'title': 'Info', 'name': 'info', 'type': 'str', 'value': '', 'readonly': True}
+        {'title': 'Info', 'name': 'info', 'type': 'str', 'value': '', 'readonly': True},
+        {'title': 'Input mode', 'name': 'imode', 'type': 'list', 'limits': DSP7265.IMODES},
+        {'title': 'Reference', 'name': 'reference', 'type': 'list', 'limits': DSP7265.REFERENCES},
+        {'title': 'Voltage mode input device', 'name': 'fet', 'type': 'list', 'limits': list(FET.keys())},
+        {'title': 'Input connector shield', 'name': 'shield', 'type': 'list', 'limits': list(SHIELD.keys())},
+        {'title': 'Coupling', 'name': 'coupling', 'type': 'list', 'limits': list(COUPLING.keys())},
+        {'title': 'Filter time constant', 'name': 'time_constant', 'type': 'list', 'limits': list(TIME_CONSTANTS.keys())},
+        {'title': 'Full-scale sensitivity', 'name': 'sensitivity', 'type': 'list',
+         'limits': list(build_dict_from_float_list(DSP7265.SENSITIVITIES, 'V'))},
+        {'title': 'Voltage (V)', 'name': 'voltage', 'type': 'float', 'limits': [0, 5], 'value': 1e-6},
+        {'title': 'Gain (dB)', 'name': 'gain', 'type': 'list', 'limits': GAIN},
     ] + comon_parameters_fun(is_multiaxes, axis_names=_axis_names, epsilon=_epsilon)
 
     def ini_attributes(self):
         self.controller = DSP7265
+        print(self.controller.imode)
 
     def get_actuator_value(self):
         """Get the current value from the hardware with scaling conversion.
@@ -66,8 +88,59 @@ class DAQ_Move_Lockin_DSP7265(DAQ_Move_base):
         param: Parameter
             A given parameter (within detector_settings) whose value has been changed by the user
         """
-        if param.name() == "a_parameter_you've_added_in_self.params":
-           self.controller.your_method_to_apply_this_param_change()
+        if param.name() == "imode":
+            self.controller.imode = param.value()
+            if param.value() == DSP7265.IMODES[0]:
+                self.settings.child('sensitivity').setLimits(
+                   list(build_dict_from_float_list([s * DSP7265.SEN_MULTIPLIER[0]
+                                                    for s in DSP7265.SENSITIVITIES],
+                                                    "V"
+                    ).keys())
+                )
+            elif param.value() == DSP7265.IMODES[1]:
+               self.settings.child('sensitivity').setLimits(
+                   list(build_dict_from_float_list([s * DSP7265.SEN_MULTIPLIER[1]
+                                                    for s in DSP7265.SENSITIVITIES],
+                                                    "A"
+                    ).keys())
+                )
+            elif param.value() == DSP7265.IMODES[2]:
+                self.settings.child('sensitivity').setLimits(
+                   list(build_dict_from_float_list([s * DSP7265.SEN_MULTIPLIER[2]
+                                                    for s in DSP7265.SENSITIVITIES],
+                                                    "A"
+                    ).keys())
+                )
+        elif param.name() == "reference":
+            self.controller.reference = param.value()
+        elif param.name() == "fet":
+            self.controller.fet = FET[param.value()]
+        elif param.name() == "shield":
+            self.controller.shield = SHIELD[param.value()]
+        elif param.name() == "coupling":
+            self.controller.coupling = COUPLING[param.value()]
+        elif param.name() == "time_constant":
+            self.controller.time_constant = TIME_CONSTANTS[param.value()]
+        elif param.name() == "sensitivity":
+            if self.settings.child('imode').value() == DSP7265.IMODES[0]:
+               self.controller.sensitivity = build_dict_from_float_list(
+                   [s * DSP7265.SEN_MULTIPLIER[0] for s in DSP7265.SENSITIVITIES],
+                   "V"
+                )[self.settings.child('sensitivity').value()]
+            if self.settings.child('imode').value() == DSP7265.IMODES[1]:
+               self.controller.sensitivity = build_dict_from_float_list(
+                   [s * DSP7265.SEN_MULTIPLIER[1] for s in DSP7265.SENSITIVITIES],
+                   "A"
+                )[self.settings.child('sensitivity').value()]
+            if self.settings.child('imode').value() == DSP7265.IMODES[2]:
+               self.controller.sensitivity = build_dict_from_float_list(
+                   [s * DSP7265.SEN_MULTIPLIER[2] for s in DSP7265.SENSITIVITIES],
+                   "A"
+                )[self.settings.child('sensitivity').value()]
+        elif param.name() == "voltage":
+            self.controller.voltage = param.value()
+        elif param.name() == "gain":
+            self.controller.gain = param.value()
         else:
             pass
 
