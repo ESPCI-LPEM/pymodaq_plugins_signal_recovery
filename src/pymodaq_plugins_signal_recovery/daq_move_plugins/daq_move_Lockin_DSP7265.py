2f2fbc6efea90edc9e19f34c84f0c8f18e3ce8dc
louisld <louisg@bloomenetwrok.fr>
2024-11-28 11:41:48 +0100

    Update documentation

--- src/pymodaq_plugins_signal_recovery/daq_move_plugins/daq_move_Lockin_DSP7265.py
+++ src/pymodaq_plugins_signal_recovery/daq_move_plugins/daq_move_Lockin_DSP7265.py
@@ -20,11 +20,15 @@ from easydict import EasyDict as edict
 
 import pyvisa
 
-def build_dict_from_float_list(time_constants: list[float], unit: str = "") -> dict:
+
+def build_dict_from_float_list(
+        time_constants: list[float],
+        unit: str = "") -> dict:
     d = {}
     for tc in time_constants:
         d[f"{tc:.2e} {unit}"] = tc
     return d
+
 
 rm = pyvisa.ResourceManager()
 VISA_RESOURCES = rm.list_resources()
@@ -35,8 +39,11 @@ COUPLING = {"AC": 0, "DC": 1}
 TIME_CONSTANTS = build_dict_from_float_list(DSP7265.TIME_CONSTANTS, "s")
 GAIN = list(range(0, 100, 10))
 
+
 class DAQ_Move_Lockin_DSP7265(DAQ_Move_base):
     """Plugin for the Signal Recovery DSP 7265 Instrument
+
+    Does not currently support differential measurement.
     """
 
     _controller_units = 'Hz'
@@ -46,75 +53,93 @@ class DAQ_Move_Lockin_DSP7265(DAQ_Move_base):
     data_actuator_type = DataActuatorType.DataActuator
 
     params = [
-        {'title': 'MultiAxes:', 'name': 'multiaxes', 'type': 'group', 'visible': is_multiaxes, 'children': [
-                     {'title': 'is Multiaxes:', 'name': 'ismultiaxes', 'type': 'bool', 'value': is_multiaxes,
-                      'default': False},
-                     {'title': 'Status:', 'name': 'multi_status', 'type': 'list', 'value': 'Master',
-                      'limits': ['Master', 'Slave']},
-                     {'title': 'Axis:', 'name': 'axis', 'type': 'list', 'limits': _axis_names},
+        {'title': 'MultiAxes:', 'name': 'multiaxes', 'type': 'group',
+         'visible': is_multiaxes, 'children': [
+             {'title': 'is Multiaxes:', 'name': 'ismultiaxes', 'type': 'bool',
+              'value': is_multiaxes, 'default': False},
+             {'title': 'Status:', 'name': 'multi_status', 'type': 'list',
+              'value': 'Master', 'limits': ['Master', 'Slave']},
+             {'title': 'Axis:', 'name': 'axis', 'type': 'list',
+              'limits': _axis_names},
 
-                 ]},
-        {'title': 'Adapter', 'name': 'adapter', 'type': 'list', 'limits': list(ADAPTERS.keys())},
-        {'title': 'VISA Address:', 'name': 'address', 'type': 'list', 'limits': VISA_RESOURCES},
-        {'title': 'Info', 'name': 'info', 'type': 'str', 'value': '', 'readonly': True},
-        {'title': 'Input mode', 'name': 'imode', 'type': 'list', 'limits': DSP7265.IMODES},
-        {'title': 'Reference', 'name': 'reference', 'type': 'list', 'limits': DSP7265.REFERENCES},
-        {'title': 'Voltage mode input device', 'name': 'fet', 'type': 'list', 'limits': list(FET.keys())},
-        {'title': 'Input connector shield', 'name': 'shield', 'type': 'list', 'limits': list(SHIELD.keys())},
-        {'title': 'Coupling', 'name': 'coupling', 'type': 'list', 'limits': list(COUPLING.keys())},
-        {'title': 'Filter time constant', 'name': 'time_constant', 'type': 'list', 'limits': list(TIME_CONSTANTS.keys())},
-        {'title': 'Full-scale sensitivity', 'name': 'sensitivity', 'type': 'list',
-         'limits': list(build_dict_from_float_list(DSP7265.SENSITIVITIES, 'V'))},
-        {'title': 'Voltage (V)', 'name': 'voltage', 'type': 'float', 'limits': [0, 5], 'value': 1e-6},
+         ]},
+        {'title': 'Adapter', 'name': 'adapter', 'type': 'list',
+         'limits': list(ADAPTERS.keys())},
+        {'title': 'VISA Address:', 'name': 'address', 'type': 'list',
+         'limits': VISA_RESOURCES},
+        {'title': 'Info', 'name': 'info', 'type': 'str', 'value': '',
+         'readonly': True},
+        {'title': 'Input mode', 'name': 'imode', 'type': 'list',
+         'limits': DSP7265.IMODES},
+        {'title': 'Reference', 'name': 'reference', 'type': 'list',
+         'limits': DSP7265.REFERENCES},
+        {'title': 'Voltage mode input device', 'name': 'fet', 'type': 'list',
+         'limits': list(FET.keys())},
+        {'title': 'Input connector shield', 'name': 'shield', 'type': 'list',
+         'limits': list(SHIELD.keys())},
+        {'title': 'Coupling', 'name': 'coupling', 'type': 'list',
+         'limits': list(COUPLING.keys())},
+        {'title': 'Filter time constant', 'name': 'time_constant',
+         'type': 'list', 'limits': list(TIME_CONSTANTS.keys())},
+        {'title': 'Full-scale sensitivity', 'name': 'sensitivity',
+         'type': 'list', 'limits':
+         list(build_dict_from_float_list(DSP7265.SENSITIVITIES, 'V'))},
+        {'title': 'Voltage (V)', 'name': 'voltage', 'type': 'float',
+         'limits': [0, 5], 'value': 1e-6},
         {'title': 'Gain (dB)', 'name': 'gain', 'type': 'list', 'limits': GAIN}
     ] + comon_parameters()
-    def ini_attributes(self):
+
+    def ini_attributes(self) -> None:
         self.controller: DSP7265 = None
 
-    def get_actuator_value(self):
+    def get_actuator_value(self) -> float:
         """Get the current value from the hardware with scaling conversion.
 
         Returns
         -------
-        float: The position obtained after scaling conversion.
+        float: The frequency obtained after scaling conversion.
         """
-        freq = DataActuator(data=self.controller.frequency)  # when writing your own plugin replace this line
+        freq = DataActuator(data=self.controller.frequency)
         freq = self.get_position_with_scaling(freq)
         return freq
 
-    def close(self):
+    def close(self) -> None:
         """Terminate the communication protocol"""
         self.controller.shutdown()
 
-    def commit_settings(self, param: Parameter):
+    def commit_settings(self, param: Parameter) -> None:
         """Apply the consequences of a change of value in the detector settings
 
         Parameters
         ----------
         param: Parameter
-            A given parameter (within detector_settings) whose value has been changed by the user
+            A given parameter (within detector_settings) whose value has been
+            changed by the user
         """
         if param.name() == "imode":
             self.controller.imode = param.value()
             if param.value() == DSP7265.IMODES[0]:
                 self.settings.child('sensitivity').setLimits(
-                   list(build_dict_from_float_list([s * DSP7265.SEN_MULTIPLIER[0]
-                                                    for s in DSP7265.SENSITIVITIES],
-                                                    "V"
+                    list(build_dict_from_float_list(
+                        [s * DSP7265.SEN_MULTIPLIER[0]
+                         for s in DSP7265.SENSITIVITIES],
+                        "V"
                     ).keys())
                 )
             elif param.value() == DSP7265.IMODES[1]:
-               self.settings.child('sensitivity').setLimits(
-                   list(build_dict_from_float_list([s * DSP7265.SEN_MULTIPLIER[1]
-                                                    for s in DSP7265.SENSITIVITIES],
-                                                    "A"
+                self.settings.child('sensitivity').setLimits(
+                    list(build_dict_from_float_list(
+                        [s * DSP7265.SEN_MULTIPLIER[1]
+                         for s in DSP7265.SENSITIVITIES],
+                        "A"
                     ).keys())
                 )
             elif param.value() == DSP7265.IMODES[2]:
                 self.settings.child('sensitivity').setLimits(
-                   list(build_dict_from_float_list([s * DSP7265.SEN_MULTIPLIER[2]
-                                                    for s in DSP7265.SENSITIVITIES],
-                                                    "A"
+                    list(build_dict_from_float_list(
+                        [s * DSP7265.SEN_MULTIPLIER[2]
+                         for s in DSP7265.SENSITIVITIES],
+                        "A"
                     ).keys())
                 )
         elif param.name() == "reference":
@@ -129,19 +154,22 @@ class DAQ_Move_Lockin_DSP7265(DAQ_Move_base):
             self.controller.time_constant = TIME_CONSTANTS[param.value()]
         elif param.name() == "sensitivity":
             if self.settings.child('imode').value() == DSP7265.IMODES[0]:
-               self.controller.sensitivity = build_dict_from_float_list(
-                   [s * DSP7265.SEN_MULTIPLIER[0] for s in DSP7265.SENSITIVITIES],
-                   "V"
+                self.controller.sensitivity = build_dict_from_float_list(
+                    [s * DSP7265.SEN_MULTIPLIER[0]
+                     for s in DSP7265.SENSITIVITIES],
+                    "V"
                 )[self.settings.child('sensitivity').value()]
             if self.settings.child('imode').value() == DSP7265.IMODES[1]:
-               self.controller.sensitivity = build_dict_from_float_list(
-                   [s * DSP7265.SEN_MULTIPLIER[1] for s in DSP7265.SENSITIVITIES],
-                   "A"
+                self.controller.sensitivity = build_dict_from_float_list(
+                    [s * DSP7265.SEN_MULTIPLIER[1]
+                     for s in DSP7265.SENSITIVITIES],
+                    "A"
                 )[self.settings.child('sensitivity').value()]
             if self.settings.child('imode').value() == DSP7265.IMODES[2]:
-               self.controller.sensitivity = build_dict_from_float_list(
-                   [s * DSP7265.SEN_MULTIPLIER[2] for s in DSP7265.SENSITIVITIES],
-                   "A"
+                self.controller.sensitivity = build_dict_from_float_list(
+                    [s * DSP7265.SEN_MULTIPLIER[2]
+                     for s in DSP7265.SENSITIVITIES],
+                    "A"
                 )[self.settings.child('sensitivity').value()]
         elif param.name() == "voltage":
             self.controller.voltage = param.value()
@@ -150,31 +178,40 @@ class DAQ_Move_Lockin_DSP7265(DAQ_Move_base):
         else:
             pass
 
-    def ini_stage(self, controller=None):
+    def ini_stage(self, controller: object = None) -> edict:
         """Actuator communication initialization
 
         Parameters
         ----------
         controller: (object)
-            custom object of a PyMoDAQ plugin (Slave case). None if only one actuator by controller (Master case)
+            custom object of a PyMoDAQ plugin (Slave case). None if only one
+            actuator by controller (Master case)
 
         Returns
         -------
         info: str
+            Id of the lockin
         initialized: bool
             False if initialization failed otherwise True
+        controller: object
+            Controller of the daq_move
         """
         try:
-            self.status.update(edict(info="", controller=None, initialized=False))
+            self.status.update(
+                edict(info="", controller=None, initialized=False)
+            )
             if (self.settings.child('multiaxes', 'ismultiaxes').value()
-                and self.settings.child('multiaxes','multi_status').value() == "Slave"):
+                    and self.settings.child('multiaxes',
+                                            'multi_status').value() == "Slave"):
                 if controller is None:
-                    raise Exception('No controller has been defined externally while this axe is a slave one')
+                    raise Exception('No controller has been defined externally'
+                                    'while this axe is a slave one')
                 else:
                     self.controller = controller
-               
             else:
-                adapter = ADAPTERS[self.settings.child('adapter').value()](self.settings.child('address').value())
+                adapter = ADAPTERS[self.settings.child('adapter').value()](
+                    self.settings.child('address').value()
+                )
                 self.controller = DSP7265(adapter)
 
             self.status.info = self.controller.id
@@ -184,46 +221,50 @@ class DAQ_Move_Lockin_DSP7265(DAQ_Move_base):
             return self.status
 
         except Exception as e:
-            self.emit_status(ThreadCommand('Update_Status', [getLineInfo() + str(e), 'log']))
+            self.emit_status(
+                ThreadCommand('Update_Status', [getLineInfo() + str(e), 'log'])
+            )
             self.status.info = getLineInfo() + str(e)
             self.status.initialized = False
             return self.status
 
-    def move_abs(self, f: DataActuator):
+    def move_abs(self, f: DataActuator) -> None:
         """ Move the actuator to the absolute target defined by value
 
         Parameters
         ----------
-        value: (float) value of the absolute target positioning
+        value: (float) value of the absolute target frequency
         """
-        f = self.check_bound(f)  #if user checked bounds, the defined bounds are applied here
+        f = self.check_bound(f)
         f = self.set_position_with_scaling(f)
         self.controller.frequency = f.value()
 
         self.target_frequency = f
         self.current_frequency = self.target_frequency
-        
 
-    def move_rel(self, f: DataActuator):
-        """ Move the actuator to the relative target actuator value defined by value
+    def move_rel(self, f: DataActuator) -> None:
+        """ Move the actuator to the relative target actuator value defined by
+        value
 
         Parameters
         ----------
-        value: (float) value of the relative target positioning
+        value: (float) value of the relative target frequency
         """
-        f = self.check_bound(self.current_frequency + f) - self.current_frequency
+        f = (self.check_bound(self.current_frequency + f)
+             - self.current_frequency)
         self.target_frequency = f + self.current_frequency
         f = self.set_position_relative_with_scaling(f)
         self.move_abs(self.target_frequency)
 
     def move_home(self):
-        """Call the reference method of the controller"""
+        """Call the reference method of the controller
 
+        Set oscillator to 1kHz
+        """
         self.move_abs(DataActuator(1e3))
 
     def stop_motion(self):
         """Stop the actuator and emits move_done signal"""
-
         pass
 
 
--- src/pymodaq_plugins_signal_recovery/daq_viewer_plugins/plugins_0D/daq_0Dviewer_Lockin_DSP7265.py
+++ src/pymodaq_plugins_signal_recovery/daq_viewer_plugins/plugins_0D/daq_0Dviewer_Lockin_DSP7265.py
@@ -1,3 +1,8 @@
+"""
+Created the 27/11/2024
+
+@author: Louis Grandvaux
+"""
 import pyvisa
 import numpy as np
 from easydict import EasyDict as edict
@@ -5,7 +10,11 @@ from easydict import EasyDict as edict
 from pymeasure.instruments.signalrecovery import DSP7265
 from pymeasure.adapters import VISAAdapter, PrologixAdapter
 
-from pymodaq.control_modules.viewer_utility_classes import DAQ_Viewer_base, comon_parameters, main
+from pymodaq.control_modules.viewer_utility_classes import (
+    DAQ_Viewer_base,
+    comon_parameters,
+    main
+)
 from pymodaq.utils.parameter import Parameter, utils
 from pymodaq.utils.data import DataFromPlugins
 from pymodaq.utils.daq_utils import ThreadCommand, getLineInfo
@@ -21,65 +30,116 @@ ADAPTERS = dict(VISA=VISAAdapter, Prologix=PrologixAdapter)
 for channel in CHANNELS:
     assert hasattr(DSP7265, channel)
 
+
 class ChannelGroup(GroupParameter):
-    
-    def __init__(self, **opts):
+    """Group Parameter listing the different output
+    """
+
+    def __init__(self, **opts) -> None:
         opts['type'] = 'dsp7265channel'
         opts['addText'] = "Add Channel"
         super().__init__(**opts)
 
-    def addNew(self):
-        """
-
+    def addNew(self) -> None:
+        """Add new channel to viewer
         """
         name_prefix = 'channel'
 
-        child_indexes = [int(par.name()[len(name_prefix) + 1:]) for par in self.children()]
+        child_indexes = [int(par.name()[len(name_prefix) + 1:])
+                         for par in self.children()]
 
         if child_indexes == []:
             newindex = 0
         else:
             newindex = max(child_indexes) + 1
 
-        child = {'title': f'Measure {newindex:02.0f}', 'name': f'{name_prefix}{newindex:02.0f}', 'type': 'itemselect',
-        'removable': True, 'value': dict(all_items=CHANNELS, selected=CHANNELS[0])}
+        child = {
+            'title': f'Measure {newindex:02.0f}',
+            'name': f'{name_prefix}{newindex:02.0f}',
+            'type': 'itemselect',
+            'removable': True,
+            'value': dict(all_items=CHANNELS, selected=CHANNELS[0])
+        }
 
         self.addChild(child)
 
+
 registerParameterType('dsp7265channel', ChannelGroup, override=True)
 
+
 class DAQ_0DViewer_Lockin_DSP7265(DAQ_Viewer_base):
+    """DAQ_viewer for DSP 7265 lockin
+    """
 
     params = [
-        {'title': 'Adapter', 'name': 'adapter', 'type': 'list', 'limits': list(ADAPTERS.keys())},
-        {'title': 'VISA Address:', 'name': 'address', 'type': 'list', 'limits': VISA_RESOURCES},
+        {'title': 'Adapter', 'name': 'adapter', 'type': 'list',
+         'limits': list(ADAPTERS.keys())},
+        {'title': 'VISA Address:', 'name': 'address', 'type': 'list',
+         'limits': VISA_RESOURCES},
         {'title': 'ID:', 'name': 'id', 'type': 'str'},
         {'title': 'Channels:', 'name': 'channels', 'type': 'dsp7270channel'}
     ] + comon_parameters
 
-    def ini_attributes(self):
+    def ini_attributes(self) -> None:
         self.controller: DSP7265 = None
 
-    def commit_settings(self, param: Parameter):
-        if param.name() in utils.iter_children(self.settings.child('channels'), []):
+    def commit_settings(self, param: Parameter) -> None:
+        """Apply the consequences of a change of value in the detector settings
+
+        Parameters
+        ----------
+        param: Parameter
+            A given parameter (within detector_settings) whose value has been
+            changed by the user
+        """
+        if param.name() in utils.iter_children(
+                self.settings.child('channels'), []):
             data = []
             for child in self.settings.child('channels').children():
                 labels = child.value()['selected']
-                data.append(DataFromPlugins(name=child.name(), data=[np.array([0]) for _ in labels],
-                                            labels=labels, dim='Data0D'))
+                data.append(
+                    DataFromPlugins(
+                        name=child.name(),
+                        data=[np.array([0]) for _ in labels],
+                        labels=labels,
+                        dim='Data0D'
+                    )
+                )
             self.data_grabed_signal_temp.emit(data)
 
-    def ini_detector(self, controller=None):
+    def ini_detector(self, controller: object = None) -> edict:
+        """Viewer communication initialization
+
+        Parameters
+        ----------
+        controller: (object, optional)
+            custom object of a PyMoDAQ plugin (Slave case). None if only one
+            actuator by controller (Master case)
+
+        Returns
+        -------
+        info: str
+            Id of the lockin
+        initialized: bool
+            False if initialization failed otherwise True
+        controller: object
+            Controller of the daq_viewer
+        """
         try:
-            self.status.update(edict(info="", controller=None, initialized=False))
+            self.status.update(
+                edict(info="", controller=None, initialized=False)
+            )
             if self.settings.child('controller_status').value() == "Slave":
                 if controller is None:
-                    raise Exception('No controller has been defined externally while this axe is a slave one')
+                    raise Exception('No controller has been defined externally'
+                                    'while this axe is a slave one')
                 else:
                     print(controller)
                     self.controller = controller
             else:
-                adapter = ADAPTERS[self.settings.child('adapter').value()](self.settings.child('address').value())
+                adapter = ADAPTERS[self.settings.child('adapter').value()](
+                    self.settings.child('address').value()
+                )
                 self.controller = DSP7265(adapter)
 
             self.status.info = self.controller.id
@@ -88,22 +148,31 @@ class DAQ_0DViewer_Lockin_DSP7265(DAQ_Viewer_base):
             return self.status
 
         except Exception as e:
-            self.emit_status(ThreadCommand('Update_Status', [getLineInfo() + str(e), 'log']))
+            self.emit_status(
+                ThreadCommand('Update_Status', [getLineInfo() + str(e), 'log'])
+            )
             self.status.info = getLineInfo() + str(e)
             self.status.initialized = False
             return self.status
 
-    def stop(self):
+    def stop(self) -> None:
         return ""
 
-    def close(self):
+    def close(self) -> None:
         pass
-    
-    def grab_data(self, Naverage=1, **kwargs):
+
+    def grab_data(self, Naverage: int = 1, **kwargs) -> None:
+        """Grab data
+
+        Args:
+            Naverage (int, optional): Number of averaging if available.
+            Defaults to 1.
+        """
         data = []
         for child in self.settings.child('channels').children():
             labels = child.value()['selected'][:]
-            subdata = [np.array([getattr(self.controller, label)]) for label in labels]
+            subdata = [np.array([getattr(self.controller, label)])
+                       for label in labels]
             data.append(DataFromPlugins(
                 name=child.name(),
                 data=subdata,
@@ -111,6 +180,7 @@ class DAQ_0DViewer_Lockin_DSP7265(DAQ_Viewer_base):
                 dim='Data0D'
             ))
         self.data_grabed_signal.emit(data)
-    
+
+
 if __name__ == '__main__':
     main(__file__, init=False)
